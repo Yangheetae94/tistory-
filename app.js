696d703987ade9085095577f099300738436146d
양희태 <htty94@gmail.com>
2023-04-01 13:02:07 +0900

    세번째

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let arr = [3, 4, 5, 6, 7];
 
 let modifiedArr = arr.map(function(props){
-    return props *3;
+    return props *2;
 });
 
-console.log(modifiedArr); // [9, 12, 15, 18, 21]
+console.log(modifiedArr); // [6, 8, 10, 12, 14]
